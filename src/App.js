df099df162d124b1c6126f6ce07f7afeb8068d8c
dineshk033 <dineshk033@gmail.com>
2021-11-12 13:49:04 +0530

    theme setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
-const App = () => <div>Parent</div>;
+import { ThemeProvider } from '@mui/material/styles';
+import { Button, CssBaseline, StyledEngineProvider } from '@mui/material';
+import themes from 'themes';
+
+const App = () => (
+    <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={themes()}>
+            <CssBaseline />
+            <div>Theme Provider</div>
+            <Button variant="contained">Outlined</Button>
+        </ThemeProvider>
+    </StyledEngineProvider>
+);
 
 export default App;
