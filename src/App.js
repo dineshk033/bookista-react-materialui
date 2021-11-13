d94ece2fdc883db1fb90e443d978094025298b49
dineshk033 <dineshk033@gmail.com>
2021-11-13 13:34:49 +0530

    Card template 3

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline, Stack, StyledEngineProvider } from '@mui/material';
 import themes from 'themes';
+import CardTemplate1 from 'component/card/cardTemplate1';
 import CardTemplate2 from 'component/card/cardTemplate2';
+import CardTemplate3 from 'component/card/cardTemplate3';
 
 const App = () => (
     <StyledEngineProvider injectFirst>
         <div style={{ margin: '2rem', backgroundColor: 'aliceblue', padding: '1rem' }}>
             <ThemeProvider theme={themes()}>
                 <CssBaseline />
-                <Stack direction="row" spacing={2}>
+                <Stack direction="column" spacing={2}>
+                    <CardTemplate1 />
                     <CardTemplate2 />
+                    <CardTemplate3 />
                 </Stack>
             </ThemeProvider>
         </div>
